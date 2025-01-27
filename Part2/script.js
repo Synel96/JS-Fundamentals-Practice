be2968119e25020e667c264ef109108fd2a0fc2d
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-01-27 08:49:44 +0100

    functions / function declaration and expression

---
+++ Part2/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/*
+let hasDriverslicense = false;
+const passTest = true ;
+
+if(passTest) hasDriverslicense = true;
+ if(hasDriverslicense) console.log("I'can drive!");
+
+ 
+
+
+ function logger() {
+    console.log("My name is Szilveszter");
+ }
+
+
+ logger();
+ logger();
+ logger();
+ 
+
+ function fruitProcessor(apples, oranges) {
+
+   console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+    return juice;
+ }
+
+ const appleJuice = fruitProcessor(5, 0);
+
+ console.log(appleJuice);
+
+ console.log(fruitProcessor(5,0));
+
+
+ const appleOrangeJuice = fruitProcessor(2, 4);
+
+ console.log(appleOrangeJuice);
+
+ 
+
+// Function declaration
+ function calcAge1(birthYear) {
+    
+    return 2037 - birthYear;
+ }
+
+ const  age1 = calcAge1(1991);
+
+ console.log(age1);
+
+ //Function expression
+ const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+
+
+ }
+
+ const age2 = calcAge2(1991);
+
+ console.log(age1, age2);
+
+ */
+
